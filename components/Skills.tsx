d46e5f981118ce79356dd2ce0ef175e56c6bb2c6
m0t0-taka <tkg.8.mthr@gmail.com>
2022-11-05 09:16:24 +0900

    Skillページ作成

---
+++ components/Skills.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Skill from "./Skill";
+
+type Props = {};
+
+export default function Skills({}: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex relative flex-col text-center md:text-left md:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Skills
+      </h3>
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+        Hoverすると習熟度が表示されます。
+      </h3>
+      <div className="mt-20 grid grid-cols-4 gap-5">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
+    </motion.div>
+  );
+}
