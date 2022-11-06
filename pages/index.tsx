688d097bcae37f7c1128490d9a7bbe45f5ad343d
m0t0-taka <tkg.8.mthr@gmail.com>
2022-11-06 09:51:18 +0900

    Projectページ作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
 
@@ -27,11 +28,13 @@ const Home: NextPage = () => {
         <WorkExperience />
       </section>
 
-      <section id="skills" className="snap-center">
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact */}
     </div>
