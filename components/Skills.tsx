789dd7064a2bb481cc4e8cf72cbcddfde1a0d81e
m0t0-taka <tkg.8.mthr@gmail.com>
2022-11-07 22:29:25 +0900

    skilページ各stackアイコン適用

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,6 +5,22 @@ import Skill from "./Skill";
 type Props = {};
 
 export default function Skills({}: Props) {
+  // 言語
+  const javaScript = "/stack/JavaScript.png";
+  const typeScript = "/stack/TypeScript.png";
+  const ruby = "/stack/Ruby.png";
+  const python = "/stack/Python.png";
+  // フレームワーク
+  const next = "/stack/Next.js.png";
+  const react = "/stack/React.png";
+  const flutter = "/stack/Flutter.png";
+  // クラウドサービス
+  const aws = "/stack/AWS.png";
+  const figma = "/stack/Figma.png";
+  const zapier = "/stack/Zapier.png";
+  const airtable = "/stack/Airtable.png";
+  const salesforce = "/stack/Salesforce.png";
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -19,18 +35,18 @@ export default function Skills({}: Props) {
         Hoverすると習熟度が表示されます。
       </h3>
       <div className="mt-20 grid grid-cols-3 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill directionLeft />
-        <Skill directionLeft />
-        <Skill directionLeft />
-        <Skill directionLeft />
-        <Skill directionLeft />
-        <Skill directionLeft />
+        <Skill stackImage={javaScript} proficiency="70" />
+        <Skill stackImage={typeScript} proficiency="30" />
+        <Skill stackImage={ruby} proficiency="50" />
+        <Skill stackImage={python} proficiency="30" />
+        <Skill stackImage={next} proficiency="50" />
+        <Skill stackImage={react} proficiency="70" />
+        <Skill directionLeft stackImage={flutter} proficiency="10" />
+        <Skill directionLeft stackImage={aws} proficiency="30" />
+        <Skill directionLeft stackImage={figma} proficiency="60" />
+        <Skill directionLeft stackImage={zapier} proficiency="90" />
+        <Skill directionLeft stackImage={airtable} proficiency="90" />
+        <Skill directionLeft stackImage={salesforce} proficiency="20" />
       </div>
     </motion.div>
   );
