4f7d7cfaadb0450c950ed53258b96637c965891c
m0t0-taka <tkg.8.mthr@gmail.com>
2022-11-14 21:57:32 +0900

    Linkのエラー解消

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,15 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type Props = {};
 
 export default function Header({}: Props) {
+  const router = useRouter();
+  const LinkToContact = () => {
+    router.push("#contact");
+  };
   return (
     // divでなくheaderとする(SEO)
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
@@ -55,13 +60,15 @@ export default function Header({}: Props) {
         }}
         className="flex flex-row items-center text-gray-300 cursor-pointer"
       >
-        <Link href="#contact">
+        <button onClick={LinkToContact}>
           <SocialIcon
             className="cursor-pointer"
             network="email"
             fgColor="gray"
             bgColor="transparent"
           />
+        </button>
+        <Link href="#contact">
           <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
             Get In Touch
           </p>
