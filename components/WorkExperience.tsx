fdc08d653a0a5af9236f9baeaf1dc7a6b61cdad5
m0t0-taka <tkg.8.mthr@gmail.com>
2022-11-14 22:36:02 +0900

    Experience margin修正

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -19,7 +19,7 @@ export default function WorkExperience({}: Props) {
           Experience
         </h3>
       </div>
-      <div className="w-full top-20 md:mt-24 flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="w-full mt-20 md:mt-24 flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         <ExperienceCardAmbient />
         <ExperienceCardJPMC />
         <ExperienceCardOkuma />
